8aa1945ec1b295c92a2710dd199d9b5fa97d8746
NessMC <contact@nessmc.fr>
2020-08-08 11:11:45 +0200

    🎉 Codecov project creatred.

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navbar from '@/components/Navbar.vue'
+
+export default Vue.extend({
+  components: {
+    Navbar
+  }
+})
+</script>
