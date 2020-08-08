3604f194b9eebb056b9f6e9ec6bdfe1f5629a859
NessMC <contact@nessmc.fr>
2020-08-08 14:02:06 +0200

    💄 Cover UI added.

--- components/Cover.vue
+++ components/Cover.vue
@@ -1,5 +1,20 @@
 <template>
-  
+  <div class="mt-40">
+    <h1 class="text-3xl font-bold text-center">
+      We've got you covered!
+    </h1>
+    <p class="w-9/12 text-center mx-auto text-gray-800 mt-4">
+      Join nearly a million developers worldwide in shipping healthier code
+    </p>
+    <div>
+      <img src="@/assets/images/background.svg" alt="">
+      <div class="container mx-auto block text-center -mt-8">
+        <a href="" class="ext-center mx-auto inline-block bg-blue-600 px-4 py-3 rounded-full text-white">
+        Get Started Now
+      </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
