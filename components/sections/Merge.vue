17dec4d5b23722793d6b0efddc7c82880b9a6fd8
NessMC <contact@nessmc.fr>
2020-08-08 12:41:54 +0200

    💄 Review section UI added.

--- components/sections/Merge.vue
+++ components/sections/Merge.vue
@@ -38,7 +38,9 @@
           </li>
         </ul>
         <a href="" class="text-blue-500 mt-4 block">
-          Learn more about report merging
+          <span class="hover:underline">
+            Learn more about report merging
+          </span>
           <span class="inline-block align-middle">
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
           </span>
---
+++ components/sections/Review.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex flex-col mt-40">
+    <div class="flex flex-auto">
+      <img src="@/assets/images/third_section.svg" class="w-9/12 mx-auto" alt="">
+    </div>
+    <div class="flex flex-auto mt-20">
+      <div class="container w-11/12 mx-auto">
+        <h1 class="text-xl font-bold">
+          Review reports seamlessly.
+        </h1>
+        <p class="w-3/4 mt-8 text-gray-800">
+          Detailed report commented directly into your pull request.
+        </p>
+        <ul class="mt-8">
+          <li class="-ml-2 text-gray-800">
+            <span class="inline-block align-middle">
+              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#77ff00" /></svg>
+            </span>
+            Customize comments in your pull requests
+          </li>
+          <li class="-mt-4 -ml-2 text-gray-800">
+            <span class="inline-block align-middle">
+              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#77ff00" /></svg>
+            </span>
+            Choose when comments get posted
+          </li>
+          <li class="-mt-4 -ml-2 text-gray-800">
+            <span class="inline-block align-middle">
+              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#77ff00" /></svg>
+            </span>
+            Smart notifications
+          </li>
+          <li class="-mt-4 -ml-2 text-gray-800">
+            <span class="inline-block align-middle">
+              <svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#77ff00" /></svg>
+            </span>
+            Coverage directly in your browser
+          </li>
+        </ul>
+        <a href="" class="text-blue-500 mt-4 block">
+          <span class="hover:underline">
+            Learn about our Sourcegraph integration
+          </span>
+          <span class="inline-block align-middle">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
