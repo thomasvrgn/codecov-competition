494d2b117d8bc63301a9392a88620bc0dcd0ff8d
NessMC <contact@nessmc.fr>
2020-08-08 13:14:28 +0200

    💄 Protection section added.

--- components/Protection.vue
+++ components/Protection.vue
@@ -1,5 +1,16 @@
 <template>
-  
+  <div class="container mt-20 mx-auto text-center">
+    <h1 class="text-xl font-bold">
+      Protection
+      <a href="" class="text-blue-600 hover:text-blue-700">
+        SOC 2
+      </a>
+    </h1>
+    <p class="w-8/12 mx-auto mt-4 text-gray-800">
+      Codecov is SOC 2 Type II certified, which means a third-party audits and attests to our practices to secure our systems and your data.
+    </p>
+    <img src="@/assets/images/protection.svg" class="w-24 mx-auto my-8" alt="">
+  </div>
 </template>
 
 <script lang="ts">
