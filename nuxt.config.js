3b297bc1c549bc4c4f644b11dcb63e431660c78c
NessMC <contact@nessmc.fr>
2020-08-15 11:17:55 +0200

    🔍 SEO Twitter and OG metas added.

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,9 +19,28 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // Default metas
+      
       { hid: 'description', name: 'description', content: 'Codecov is the leading, dedicated code coverage solution. Try Codecov for free now to help your developers find untested code and deploy changes with confidence.' },
+      { hid: 'title', name: 'title', content: 'Code Coverage Done Right | Codecov' },
+
+      // OG Metas
+
       { hid: 'og:image', property: 'og:image', content: 'https://codecov.io/static/header-logo-pink.png' },
-      { hid: 'og:title', property: 'og:title', content: 'Code Coverage Done Right | Codecov'}
+      { hid: 'og:title', property: 'og:title', content: 'Code Coverage Done Right | Codecov' },
+      { hid: 'og:url', property: 'og:url', content: 'https://codecov-ness.netlify.app/' },
+      { hid: 'og:description', property: 'og:description', content: 'Codecov is the leading, dedicated code coverage solution. Try Codecov for free now to help your developers find untested code and deploy changes with confidence.' },
+      { hid: 'og:image', property: 'og:image', content: 'https://codecov.io/static/header-logo-pink.png' },
+
+      // Twitter Metas
+
+      { hid: 'twitter:card', property: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:url', property: 'twitter:url', content: 'https://codecov-ness.netlify.app/' },
+      { hid: 'twitter:title', property: 'twitter:title', content: 'Code Coverage Done Right | Codecov' },
+      { hid: 'twitter:description', property: 'twitter:description', content: 'Codecov is the leading, dedicated code coverage solution. Try Codecov for free now to help your developers find untested code and deploy changes with confidence.' },
+      { hid: 'twitter:image', property: 'twitter:image', content: 'https://codecov.io/static/header-logo-pink.png' },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
